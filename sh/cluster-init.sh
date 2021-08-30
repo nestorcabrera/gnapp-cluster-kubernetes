d1712e068e7740eb071ffc8cb552922d278e8ff9
Nestor Cabrera <nic.net9@gmail.com>
2021-08-30 14:45:12 +0000

    All work it!

--- sh/cluster-init.sh
+++ sh/cluster-init.sh
@@ -21,21 +21,34 @@ helm upgrade --install ingress-nginx ingress-nginx/ingress-nginx \
      #--set controller.service.annotations."kubernetes.io/ingress.global-static-ip-name"="test-dev" \
      #--set controller.service.loadBalancerIP=34.107.141.150 \
 
+sleep 60
+
 helm upgrade --install cert-manager jetstack/cert-manager \
     --namespace cert-manager \
     --version v1.5.3 \
     --set installCRDs=true
 
+kubectl apply -f ../common/
+
 helm upgrade --install gitlab gitlab/gitlab \
     --timeout 600s \
-    --set global.hosts.domain=git.dev.gasneiva.com \
-    --set global.hosts.externalIP=35.201.72.51 \
-    --set certmanager-issuer.email=nestorcabrera@gasneiva.com \
     --set gitlab-runner.runners.privileged=true \
+    --set nginx-ingress.enabled=false \
+    --set global.ingress.class=nginx \
+    --set certmanager.install=false \
+    --set global.ingress.configureCertmanager=false \
+    --set global.hosts.domain=gnapp.dev.gasneiva.com \
+    --set global.ingress.tls.secretName=gitlab.gnapp.dev.gasneiva.com-tls \
+    --set registry.ingress.tls.secretName=registry.gnapp.dev.gasneiva.com-tls \
     --version 5.2.1 \
     --namespace gitlab
+    #--set global.hosts.domain=git.dev.gasneiva.com  
+    #--set global.hosts.externalIP=35.201.72.51 \
+    #--set certmanager-issuer.email=nestorcabrera@gasneiva.com \
 
-kubectl apply -f ../common/
+sleep 60
+
+kubectl -n gitlab get secret gitlab-gitlab-initial-root-password -ojsonpath='{.data.password}' | base64 --decode ; echo
 
 helm upgrade --install app-test ../app-test/
 
--- sh/cluster-uninstall.sh
+++ sh/cluster-uninstall.sh
@@ -2,3 +2,7 @@ helm uninstall app-test
 helm uninstall gitlab -n gitlab
 helm uninstall cert-manager -n cert-manager
 helm uninstall ingress-nginx -n ingress-nginx
+
+kubectl delete ns gitlab
+kubectl delete ns cert-manager
+kubectl delete ns ingress-nginx
